14b27c74547a56f6abc15725c703a8023bb72b91
Andrew Stone <andrew@clovar.com>
2015-01-09 11:23:54 -0500

    Turn all assertions into expressions
    Without this, it's possible for some of the assertions not to be
    terminated with a semicolon (we all make mistakes), and that's just
    weird.

--- paratec.h
+++ paratec.h
@@ -128,7 +128,7 @@
 #define pt(cond) \
 	if (!(cond)) { \
 		pt_fail("`%s` failed", #cond); } \
-	pt_mark();
+	pt_mark()
 
 /**
  * Like pt(), but allows you to give your own message.
@@ -136,15 +136,15 @@
 #define pt_msg(cond, msg, ...) \
 	if (!(cond)) { \
 		pt_fail(msg, ##__VA_ARGS__); } \
-	pt_mark();
+	pt_mark()
 
 /**
  * String assertions with custom messages
  */
-#define pt_str_msg(a, op, b, msg, ...) { \
+#define pt_str_msg(a, op, b, msg, ...) ({ \
 	const char* __pt_a = (a); \
 	const char* __pt_b = (b); \
-	pt_msg(0 op strcmp(__pt_a, __pt_b), msg, ##__VA_ARGS__); }
+	pt_msg(0 op strcmp(__pt_a, __pt_b), msg, ##__VA_ARGS__); })
 #define pt_seq_msg(a, b, msg, ...) pt_str(a, ==, b, msg, ##__VA_ARGS__)
 #define pt_sne_msg(a, b, msg, ...) pt_str(a, !=, b, msg, ##__VA_ARGS__)
 #define pt_ss_msg(a, b, msg, ...) {\
@@ -155,9 +155,9 @@
 /**
  * String assertions
  */
-#define pt_str(a, op, b) { \
+#define pt_str(a, op, b) ({ \
 	pt_str_msg(a, op, b, \
-		"Expected `%s` %s `%s`", __pt_a, #op, __pt_b); }
+		"Expected `%s` %s `%s`", __pt_a, #op, __pt_b); })
 #define pt_seq(a, b) pt_str(a, ==, b)
 #define pt_sne(a, b) pt_str(a, !=, b)
 #define pt_ss(a, b) pt_ss_msg(a, b, "`%s` does not contain `%s`", __pt_a, __pt_b)
@@ -165,30 +165,30 @@
 /**
  * Binary assertions with custom messages
  */
-#define pt_mem_msg(a, op, b, len, msg, ...) { \
+#define pt_mem_msg(a, op, b, len, msg, ...) ({ \
 	const void* __pt_a = (a); \
 	const void* __pt_b = (b); \
 	const size_t n = (len); \
-	pt_msg(0 op memcmp(__pt_a, __pt_b, n), msg, ##__VA_ARGS__); }
+	pt_msg(0 op memcmp(__pt_a, __pt_b, n), msg, ##__VA_ARGS__); })
 #define pt_meq_msg(a, b, len, msg, ...) pt_mem(a, ==, b, len, msg, ##__VA_ARGS__)
 #define pt_mne_msg(a, b, len, msg, ...) pt_mem(a, !=, b, len, msg, ##__VA_ARGS__)
 
 /**
  * Binary assertions
  */
-#define pt_mem(a, op, b, len) { \
+#define pt_mem(a, op, b, len) ({ \
 	pt_mem_msg(a, op, b, len, \
-		"Expected `%s` %s `%s`", #a, #op, #b); }
+		"Expected `%s` %s `%s`", #a, #op, #b); })
 #define pt_meq(a, b, len) pt_mem(a, ==, b, len)
 #define pt_mne(a, b, len) pt_mem(a, !=, b, len)
 
 /**
  * Pointer assertions with custom messages
  */
-#define pt_ptr_msg(a, op, b, msg, ...) { \
+#define pt_ptr_msg(a, op, b, msg, ...) ({ \
 	const void *__pt_a = (a); \
 	const void *__pt_b = (b); \
-	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); }
+	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); })
 #define pt_peq_msg(a, b, msg, ...) pt_ptr_msg(a, ==, b, msg, ##__VA_ARGS__)
 #define pt_pne_msg(a, b, msg, ...) pt_ptr_msg(a, !=, b, msg, ##__VA_ARGS__)
 #define pt_pgt_msg(a, b, msg, ...) pt_ptr_msg(a, >, b, msg, ##__VA_ARGS__)
@@ -199,9 +199,9 @@
 /**
  * Pointer assertions
  */
-#define pt_ptr(a, op, b) { \
+#define pt_ptr(a, op, b) ({ \
 	pt_ptr_msg(a, op, b, \
-		"Expected %p %s %p", __pt_a, #op, __pt_b); }
+		"Expected %p %s %p", __pt_a, #op, __pt_b); })
 #define pt_peq(a, b) pt_ptr(a, ==, b)
 #define pt_pne(a, b) pt_ptr(a, !=, b)
 #define pt_pgt(a, b) pt_ptr(a, >, b)
@@ -212,10 +212,10 @@
 /**
  * Integer assertions with custom messages
  */
-#define pt_int_msg(a, op, b, msg, ...) { \
+#define pt_int_msg(a, op, b, msg, ...) ({ \
 	const int64_t __pt_a = (a); \
 	const int64_t __pt_b = (b); \
-	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); }
+	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); })
 #define pt_eq_msg(a, b, msg, ...) pt_int_msg(a, ==, b, msg, ##__VA_ARGS__)
 #define pt_ne_msg(a, b, msg, ...) pt_int_msg(a, !=, b, msg, ##__VA_ARGS__)
 #define pt_gt_msg(a, b, msg, ...) pt_int_msg(a, >, b, msg, ##__VA_ARGS__)
@@ -226,10 +226,10 @@
 /**
  * Integer assertions
  */
-#define pt_int(a, op, b) { \
+#define pt_int(a, op, b) ({ \
 	pt_int_msg(a, op, b, \
 		"Expected %'" PRId64 " %s %'" PRId64 " (off by %'" PRId64 ")", \
-			__pt_a, #op, __pt_b, __pt_b - __pt_a); }
+			__pt_a, #op, __pt_b, __pt_b - __pt_a); })
 #define pt_eq(a, b) pt_int(a, ==, b)
 #define pt_ne(a, b) pt_int(a, !=, b)
 #define pt_gt(a, b) pt_int(a, >, b)
@@ -240,10 +240,10 @@
 /**
  * Unsigned integer assertions with custom messages
  */
-#define pt_uint_msg(a, op, b, msg, ...) { \
+#define pt_uint_msg(a, op, b, msg, ...) ({ \
 	const uint64_t __pt_a = (a); \
 	const uint64_t __pt_b = (b); \
-	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); }
+	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); })
 #define pt_ueq_msg(a, b, msg, ...) pt_uint_msg(a, ==, b, msg, ##__VA_ARGS__)
 #define pt_une_msg(a, b, msg, ...) pt_uint_msg(a, !=, b, msg, ##__VA_ARGS__)
 #define pt_ugt_msg(a, b, msg, ...) pt_uint_msg(a, >, b, msg, ##__VA_ARGS__)
@@ -254,10 +254,10 @@
 /**
  * Unsigned integer assertions
  */
-#define pt_uint(a, op, b) { \
+#define pt_uint(a, op, b) ({ \
 	pt_uint_msg(a, op, b, \
 		"Expected %'" PRIu64 " %s %'" PRIu64 " (off by %'" PRId64 ")", \
-			__pt_a, #op, __pt_b, __pt_b - __pt_a); }
+			__pt_a, #op, __pt_b, __pt_b - __pt_a); })
 #define pt_ueq(a, b) pt_uint(a, ==, b)
 #define pt_une(a, b) pt_uint(a, !=, b)
 #define pt_ugt(a, b) pt_uint(a, >, b)
@@ -268,10 +268,10 @@
 /**
  * Floating point assertions with custom messages
  */
-#define pt_f_msg(a, op, b, msg, ...) { \
+#define pt_f_msg(a, op, b, msg, ...) ({ \
 	const double __pt_a = (a); \
 	const double __pt_b = (b); \
-	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); }
+	pt_msg(__pt_a op __pt_b, msg, ##__VA_ARGS__); })
 #define pt_feq_msg(a, b, msg, ...) pt_fint_msg(a, ==, b, msg, ##__VA_ARGS__)
 #define pt_fne_msg(a, b, msg, ...) pt_fint_msg(a, !=, b, msg, ##__VA_ARGS__)
 #define pt_fgt_msg(a, b, msg, ...) pt_fint_msg(a, >, b, msg, ##__VA_ARGS__)
@@ -282,10 +282,10 @@
 /**
  * Floating point assertions
  */
-#define pt_f(a, op, b) { \
+#define pt_f(a, op, b) ({ \
 	pt_f_msg(a, op, b, \
 		"Expected %'f %s %'f (off by %'f)", \
-			__pt_a, #op, __pt_b, __pt_b - __pt_a); }
+			__pt_a, #op, __pt_b, __pt_b - __pt_a); })
 #define pt_feq(a, b) pt_f(a, ==, b)
 #define pt_fne(a, b) pt_f(a, !=, b)
 #define pt_fgt(a, b) pt_f(a, >, b)
@@ -296,10 +296,10 @@
 /**
  * For asserting return values that also set errno.
  */
-#define pt_eno(a, b) { \
+#define pt_eno(a, b) ({ \
 	pt_eq_msg(a, b, \
 		"Expected %'" PRId64 " == %'" PRId64 ". Error %d: %s", \
-			__pt_a, __pt_b, errno, strerror(errno)); }
+			__pt_a, __pt_b, errno, strerror(errno)); })
 
 /**
  * When testing network services, it's useful to have a unique port for the
